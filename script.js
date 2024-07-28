cf04f1e61a3cf0fb9cf5c31b085f2d6eb90f18e3
unknown <brendontran007@gmail.com>
2024-07-28 11:36:41 -0400

    Updated program, added and modified files

--- script.js
+++ script.js
@@ -22,16 +22,41 @@ const answersArray = [
 ]
 
 const shakeAudio = new Audio('ball-shake.wav');
+const errorAudio = new Audio('xp-error.mp3');
 
 // Element Declarations
 const ballImg = document.getElementById('ball-img');
 const answerTxt = document.getElementById('answer-txt');
-const questionTxt = document.getElementById('question-txt');
+const questionInput = document.getElementById('question-input');
+const nameInput = document.getElementById('name-input');
 const submitBtn = document.getElementById('submit-btn');
+const clearNameBtn = document.getElementById('nclear-btn');
+const clearQuesBtn = document.getElementById('qclear-btn');
+const clearAllBtn = document.getElementById('aclear-btn');
 
 // Event Listener
 ballImg.addEventListener('click', function() {
-  const answerIndex = Math.floor(Math.random() * answersArray.length);
-  answerTxt.textContent = answersArray[answerIndex];
-  shakeAudio.play()
+  if (nameInput.value === '' || questionInput.value === '') {
+    answerTxt.textContent = 'One of the fields (name or question) is missing.'
+    errorAudio.play();
+    setTimeout(() => { answerTxt.textContent = '[Answer will be displayed here]' }, 2000);
+  } else {
+    const answerIndex = Math.floor(Math.random() * answersArray.length);
+    answerTxt.textContent = answersArray[answerIndex];
+    shakeAudio.play();
+    setTimeout(() => { answerTxt.textContent = '[Answer will be displayed here]' }, 5000);
+  }
+})
+
+clearNameBtn.addEventListener('click', function() {
+  nameInput.value = '';
+})
+
+clearQuesBtn.addEventListener('click', function() {
+  questionInput.value = '';
+})
+
+clearAllBtn.addEventListener('click', function() {
+  nameInput.value = '';
+  questionInput.value = '';
 })
